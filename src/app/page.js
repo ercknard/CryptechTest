9ade2a56f978cb78e50d9122db806a73f8dfad67
Ercknard Guren <79645928+ercknard@users.noreply.github.com>
2023-04-23 05:16:09 +0800

    icon

--- src/app/page.js
+++ src/app/page.js
@@ -4,6 +4,7 @@ import styles from './page.module.css'
 export default function Home() {
   return (
     <main className={styles.main}>
+      <div className={styles.home_section}>
       <div className={styles.description}>
         <p>
           Get started by editing&nbsp;
@@ -89,6 +90,7 @@ export default function Home() {
             Instantly deploy your Next.js site to a shareable URL with Vercel.
           </p>
         </a>
+        </div>
       </div>
     </main>
   )
