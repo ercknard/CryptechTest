ad6282f294c8f0e800d3d312e4cb50a8f0dba3c1
Ercknard Guren <79645928+ercknard@users.noreply.github.com>
2023-05-19 01:19:07 +0800

    Update Footer.jsx

--- components/sections/Footer.jsx
+++ components/sections/Footer.jsx
@@ -6,27 +6,44 @@ const Footer = () => (
             <footer className="main-footer">
         <ul className="social-icon">
             <li className="social-icon__item">
-            <a className="social-icon__link" href="#">
+
+            <a 
+            title="Facebook" 
+            className="social-icon__link" 
+            href="#">
                 <ion-icon name="logo-facebook" />
             </a>
+
             </li>
             <li className="social-icon__item">
-            <a className="social-icon__link" href="https://twitter.com/CryptechCraft">
+
+            <a 
+            title="Twitter" 
+            className="social-icon__link" 
+            href="https://twitter.com/CryptechCraft">
                 <ion-icon name="logo-twitter" />
             </a>
+
             </li>
             <li className="social-icon__item">
-            <a
-                className="social-icon__link"
-                href="https://discord.com/invite/WxvrzzPFAH"
-            >
+
+            <a 
+            title="Discord"
+            className="social-icon__link"
+            href="https://discord.com/invite/WxvrzzPFAH">
                 <ion-icon name="logo-discord" />
             </a>
+
             </li>
             <li className="social-icon__item">
-            <a className="social-icon__link" href="#">
+
+            <a 
+            title="Instagram" 
+            className="social-icon__link" 
+            href="#">
                 <ion-icon name="logo-instagram" />
             </a>
+            
             </li>
         </ul>
         <h3 className="main-footer-text text-motion-index">
