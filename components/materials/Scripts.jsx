b3e6e56974b487a655e6355eae1acb285fc44e7c
Ercknard Guren <79645928+ercknard@users.noreply.github.com>
2023-05-18 19:12:35 +0800

    Update Scripts.jsx

--- components/materials/Scripts.jsx
+++ components/materials/Scripts.jsx
@@ -7,9 +7,8 @@ export default function AllScripts() {
         <Script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js" as="script" type="text/javascript" />
         {/* <Script rel="preload" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js" as="script" type='module' /> */}
         <Script src="/assets/scripts/AOS.js" as="script" type="text/javascript" />
-        <Script src="/assets/scripts/NPC.js" as="script" type="text/javascript" />
+        <Script src="/assets/scripts/npc.js" as="script" type="text/javascript" />
         {/* <Script src="/assets/scripts/typing.js" type="text/javascript" />
-        <Script src="/assets/scripts/npc.js" type="text/javascript" />
         <Script src="/assets/scripts/scroll.js" type="text/javascript" />
         <Script src="/assets/scripts/parallax.js" type="text/javascript" />
         <Script src="/assets/scripts/to-parallax.js" type="text/javascript" />
