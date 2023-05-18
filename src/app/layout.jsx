193a772e6b5986b74133b567e4db860bc25edaae
Ercknard Guren <79645928+ercknard@users.noreply.github.com>
2023-05-19 00:14:26 +0800

    Update layout.jsx

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -27,7 +27,7 @@ const RootLayout = ({ children }) => (
       <link rel="icon" href="/assets/images/pack.png" />
     </head>
     <body>
-    <CustomLoader />
+    {/* <CustomLoader /> */}
       <LandingPage />
         <PepeSpace />
           <Gate />
