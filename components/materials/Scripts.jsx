b1387023c3ad2727f0261c51936fc38f85c8c220
Ercknard Guren <79645928+ercknard@users.noreply.github.com>
2023-05-19 01:08:08 +0800

    Update Scripts.jsx

--- components/materials/Scripts.jsx
+++ components/materials/Scripts.jsx
@@ -4,12 +4,12 @@ export default function AllScripts() {
     return (
 
         <>
-        <Script rel="preload" src="/assets/scripts/customloader.js" as="script" type="text/javascript"/>
+        <Script rel="preload" src="/assets/scripts/customloader.js" as="script" type="text/javascript" />
         <Script rel="preload" src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js" as="script" type="text/javascript" />
         <Script rel="preload" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js" as="script" type='module' />
         <Script rel="preload" src="/assets/scripts/AOS.js" as="script" type="text/javascript" />
         <Script rel="preload" src="/assets/scripts/npc.js" as="script" type="text/javascript" />
-        <Script rel="preload" src="/assets/scripts/theme-modal.js" type="text/javascript" />
+        <Script rel="preload" src="/assets/scripts/theme-modal.js" as="script" type="text/javascript" />
         {/* <Script src="/assets/scripts/typing.js" type="text/javascript" />
         <Script src="/assets/scripts/scroll.js" type="text/javascript" />
         <Script src="/assets/scripts/parallax.js" type="text/javascript" />
