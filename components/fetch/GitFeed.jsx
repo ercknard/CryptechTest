8de4d840faebffc8dab833a10104e6955a484f51
Ercknard Guren <79645928+ercknard@users.noreply.github.com>
2023-05-23 05:04:20 +0800

    gitfeed tweaks

--- components/fetch/GitFeed.jsx
+++ components/fetch/GitFeed.jsx
@@ -48,11 +48,13 @@ const GitFeed = ({ username }) => {
         ))))}
         </div> */}
         <div className="feeds">
+            <h3 className="feed-starter"> Start: </h3>
           <ul>
         {feed.map((commits) => (
             <li key={commits.sha}> {commits.id} <a className="a-button" href={commits.html_url} target='_blank'> <div className='git-feed banner mod push-color'><div className="avatar-feed"> <img src={commits.committer.avatar_url} width={50} height={50} /> {commits.commit.committer.name} <div className="sha-feed"> Sha: {truncateSHA(commits.sha)}... | {commits.commit.message} - {commits.commit.committer.name} </div> </div> <div className="time-feed"> {commits.commit.committer.date}</div></div></a></li>
             ))}
             </ul>
+            <h3 className="feed-starter"> End: </h3>
             </div>
     </section>
 
