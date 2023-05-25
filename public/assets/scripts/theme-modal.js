652743c6ed23de47cf27cd4d62c9c7a12d31b2d6
Ercknard Guren <79645928+ercknard@users.noreply.github.com>
2023-05-25 20:48:05 +0800

    Update theme-modal.js

--- public/assets/scripts/theme-modal.js
+++ public/assets/scripts/theme-modal.js
@@ -1,9 +1,9 @@
 
-function toNoneModal() {
-  document.getElementById("modalforbid").style.display = "none";
-  document.getElementById("modalforbid").style.visibility = "hidden";
-  document.getElementById("modalforbid").style.opacity = "0";
-}
+// function toNoneModal() {
+//   document.getElementById("modalforbid").style.display = "none";
+//   document.getElementById("modalforbid").style.visibility = "hidden";
+//   document.getElementById("modalforbid").style.opacity = "0";
+// }
 
 var modal = document.getElementById("myModal");
 const themeDelay = 2000;
