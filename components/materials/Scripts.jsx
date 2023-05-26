dd87a72e68785322438635587b8bf2e25135d0e3
Ercknard Guren <79645928+ercknard@users.noreply.github.com>
2023-05-26 18:30:39 +0800

    Update Scripts.jsx

--- components/materials/Scripts.jsx
+++ components/materials/Scripts.jsx
@@ -6,7 +6,7 @@ export default function AllScripts() {
         <>
         <Script rel="preload" src="/assets/scripts/customloader.js" as="script" type="text/javascript" />
         <Script rel="preload" src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js" as="script" type="text/javascript" />
-        <Script rel="preload" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js" as="script" type='module' />
+        <Script src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js" as="script" type='module' />
         <Script rel="preload" src="/assets/scripts/AOS.js" as="script" type="text/javascript" />
         <Script rel="preload" src="/assets/scripts/npc.js" as="script" type="text/javascript" />
         <Script rel="preload" src="/assets/scripts/theme-modal.js" as="script" type="text/javascript" />
@@ -14,7 +14,7 @@ export default function AllScripts() {
         <Script rel="preload" src="/assets/scripts/scroll.js" as="script" type="text/javascript" />
         <Script rel="preload" src="/assets/scripts/tab.js" as="script" type="text/javascript" />
         <Script rel="preload" src="/assets/scripts/themes.js" as="script" type="text/javascript" />
-        <Script rel="preload" src="/assets/scripts/colors.mjs" as="script" type='module' />
+        {/* <Script src="/assets/scripts/colors.mjs" as="script" type='module' /> */}
         {/* <Script src="/assets/scripts/typing.js" as="script" type="text/javascript" />
         <Script src="/assets/scripts/to-parallax.js" type="text/javascript" /> */}
         </>
