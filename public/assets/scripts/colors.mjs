636289bd00952ed1522c281245db56f459259e7b
Ercknard Guren <79645928+ercknard@users.noreply.github.com>
2023-05-26 18:26:09 +0800

    Update colors.mjs

--- public/assets/scripts/colors.mjs
+++ public/assets/scripts/colors.mjs
@@ -1,8 +1,8 @@
-import supportsColor from './node_modules/supports-color/index.js';
+// import supportsColor from './node_modules/supports-color/index.js';
 
 // Check if the terminal/console supports color
-if (supportsColor) {
-  console.log('Color is supported in this environment.');
-} else {
-  console.log('Color is not supported in this environment.');
-}
+// if (supportsColor) {
+//   console.log('Color is supported in this environment.');
+// } else {
+//   console.log('Color is not supported in this environment.');
+// }
