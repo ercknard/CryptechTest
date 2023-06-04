f97d712f85a0a712a30c7f18a384c1d7e7e1ff4a
Ercknard Guren <79645928+ercknard@users.noreply.github.com>
2023-06-05 06:17:39 +0800

    news

--- components/fetch/News.jsx
+++ components/fetch/News.jsx
@@ -45,7 +45,7 @@ const announcement = () => {
     <div>
       {announcement.slice(0, 3).map((announcement) => (
         <div className="sos" key={announcement.node_id}>
-          <ul className="sos-1 mod">
+          <ul className="sos-1 mod news-selected-color">
             <li>
               <h3 className="sos-2">
                 {" "}
