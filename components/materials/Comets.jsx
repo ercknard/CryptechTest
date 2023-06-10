5341c55549586814e55290db9bb7e7764cdcdc26
Ercknard Guren <79645928+ercknard@users.noreply.github.com>
2023-06-10 10:18:02 +0800

    12

--- components/materials/Comets.jsx
+++ components/materials/Comets.jsx
@@ -5,9 +5,9 @@ const Comets = () => (
     <Image
       alt="Earth"
       className="ship"
-      // data-aos="zoom-in-left"
-      // data-aos-duration={3000}
-      // data-aos-anchor-placement="bottom-bottom"
+      data-aos="zoom-in-left"
+      data-aos-duration={3000}
+      data-aos-anchor-placement="bottom-bottom"
       src="/assets/images/asteroid-1-1.webp"
       width={1024}
       height={1024}
@@ -15,9 +15,9 @@ const Comets = () => (
     <Image
       alt="Earth"
       className="ship"
-      // data-aos="zoom-in-right"
-      // data-aos-duration={3000}
-      // data-aos-anchor-placement="bottom-bottom"
+      data-aos="zoom-in-right"
+      data-aos-duration={3000}
+      data-aos-anchor-placement="bottom-bottom"
       src="/assets/images/asteroid-2-2.webp"
       width={1024}
       height={1024}
@@ -25,9 +25,9 @@ const Comets = () => (
     <Image
       alt="Earth"
       className="ship"
-      // data-aos="zoom-in-left"
-      // data-aos-duration={3000}
-      // data-aos-anchor-placement="bottom-bottom"
+      data-aos="zoom-in-left"
+      data-aos-duration={3000}
+      data-aos-anchor-placement="bottom-bottom"
       src="/assets/images/asteroid-3-2.webp"
       width={1024}
       height={1024}
@@ -35,9 +35,9 @@ const Comets = () => (
     <Image
       alt="Earth"
       className="ship"
-      // data-aos="zoom-in-right"
-      // data-aos-duration={3000}
-      // data-aos-anchor-placement="bottom-bottom"
+      data-aos="zoom-in-right"
+      data-aos-duration={3000}
+      data-aos-anchor-placement="bottom-bottom"
       src="/assets/images/asteroid-4-2.webp"
       width={1024}
       height={1024}
