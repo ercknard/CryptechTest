b03b92ccc3e94bd3c82d88dbfed009f726bae959
Ercknard Guren <79645928+ercknard@users.noreply.github.com>
2023-06-11 09:55:14 +0800

    Update TutorialModal.jsx

--- components/materials/play/TutorialModal.jsx
+++ components/materials/play/TutorialModal.jsx
@@ -28,7 +28,7 @@ const TutorialModal = ({ closeModal }) => {
               Press the Register option. Once you have done that, the client
               will proceed to download the necessary files and thats it.{" "}
               <p className="credentials">
-                Welcome to our server and good luck landing.
+                Welcome to our server and good luck landing. -Naenshiko
               </p>
             </p>
           </div>
