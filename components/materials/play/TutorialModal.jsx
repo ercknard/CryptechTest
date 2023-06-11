364fc48a1691db5f4dbb33a6271342316ea0c345
Ercknard Guren <79645928+ercknard@users.noreply.github.com>
2023-06-11 10:00:06 +0800

    12

--- components/materials/play/TutorialModal.jsx
+++ components/materials/play/TutorialModal.jsx
@@ -12,7 +12,7 @@ const TutorialModal = ({ closeModal }) => {
       <div id="modalforbid" className="modal-overlay-tut" onClick={handleClose}>
         <div className="modal-vid-tut" onClick={(e) => e.stopPropagation()}>
           <div className="tut-mem">
-            <h2>Join CryptechTest Now!</h2>
+            <h2 className="tut-h3">Join CryptechTest Now!</h2>
             <p className="details tut-mine">
               Its simple, to join our server first download the client which is
               free on their page:{" "}
@@ -28,7 +28,7 @@ const TutorialModal = ({ closeModal }) => {
               Press the Register option. Once you have done that, the client
               will proceed to download the necessary files and thats it.{" "}
               <p className="credentials">
-                Welcome to our server and good luck landing. -Naenshiko
+                Welcome to our server and good luck landing. -Karen
               </p>
             </p>
           </div>
