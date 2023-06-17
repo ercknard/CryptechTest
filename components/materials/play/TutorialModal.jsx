ea6343a689343626df6da770ebad3f31ce5b7d5d
Ercknard Guren <79645928+ercknard@users.noreply.github.com>
2023-06-18 06:43:13 +0800

    play

--- components/materials/play/TutorialModal.jsx
+++ components/materials/play/TutorialModal.jsx
@@ -12,25 +12,28 @@ const TutorialModal = ({ closeModal }) => {
       <div id="modalforbid" className="modal-overlay-tut" onClick={handleClose}>
         <div className="modal-vid-tut" onClick={(e) => e.stopPropagation()}>
           <div className="tut-mem">
-            <h2 className="tut-h3">Join CryptechTest Now!</h2>
-            <p className="details tut-mine">
-              Its simple, to join our server first download the client which is
-              free on their page:{" "}
-              <Link href="https://www.minetest.net/" target="_blank">
-                MINETEST
-              </Link>{" "}
-              .
-            </p>
-            <p className="details tut-mine">
-              Open Minetest client, choose the join game tab. Put CryptectTest
-              Credentials: <p className="credentials">IP: cryptechcraft.xyz</p>{" "}
-              <p className="credentials">Port: 30000</p>
-              Press the Register option. Once you have done that, the client
-              will proceed to download the necessary files and thats it.{" "}
-              <p className="credentials">
-                "Welcome to our server and good luck landing." -Karen
+            <div>
+              <h2 className="tut-h3">Join CryptechTest Now!</h2>
+              <p className="details tut-mine">
+                Its simple, to join our server first download the client which
+                is free on their page:{" "}
+                <Link href="https://www.minetest.net/" target="_blank">
+                  MINETEST
+                </Link>{" "}
+                .
               </p>
-            </p>
+              <p className="details tut-mine">
+                Open Minetest client, choose the join game tab. Put CryptectTest
+                Credentials:{" "}
+                <p className="credentials">IP: cryptechcraft.xyz</p>{" "}
+                <p className="credentials">Port: 30000</p>
+                Press the Register option. Once you have done that, the client
+                will proceed to download the necessary files and thats it.{" "}
+                <p className="credentials">
+                  "Welcome to our server and good luck landing." -Karen
+                </p>
+              </p>
+            </div>
           </div>
           <div className="close-button" onClick={handleClose}>
             Close
